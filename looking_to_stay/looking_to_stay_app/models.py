2db9a309dcc8e4bb37103c1ca5a62a89f7b58d82
GalijotasE <galijotase@gmail.com>
2022-11-29 20:18:03 +0200

    click here to see recent changes

--- looking_to_stay/looking_to_stay_app/models.py
+++ looking_to_stay/looking_to_stay_app/models.py
@@ -13,3 +13,29 @@ class Category(models.Model):
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
 
+class Hotel(models.Model):
+    id = models.AutoField('Unique ID', primary_key=True)
+    hotel_name = models.CharField('Hotel Name', max_length=100, help_text="Please enter a name of the Hotel")
+    description = models.TextField('Description', max_length=10000,
+        help_text='Please enter a description of the hotel, consisting up to 10000 words.')
+    company_id = models.ForeignKey(
+        Company, on_delete=models.SET_NULL,
+        null=True, blank=True,
+    )
+    city_id = models.ForeignKey(
+        City, on_delete=models.SET_NULL,
+        null=True, blank=True,
+    )
+    category_id = models.ForeignKey(
+        Category, on_delete=models.SET_NULL,
+        null=True, blank=True,
+    )
+    cover_photo = models.ImageField('Cover Photo', upload_to='covers', blank=True, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.hotel_name}"
+
+    class Meta:
+        ordering = ['hotel_name', 'company_id']
+        verbose_name = 'Hotel'
+        verbose_name_plural = 'Hotels'
