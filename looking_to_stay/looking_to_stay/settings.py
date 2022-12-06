7b25ae02925743f5e9509a79172662916ba0ac20
GalijotasE <galijotase@gmail.com>
2022-12-06 16:03:09 +0200

    click here to see recent changes

--- looking_to_stay/looking_to_stay/settings.py
+++ looking_to_stay/looking_to_stay/settings.py
@@ -129,6 +129,7 @@ MEDIA_ROOT = BASE_DIR.joinpath('media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # vietoj console - smtp
 EMAIL_HOST = 'smtp.gmail.com'
