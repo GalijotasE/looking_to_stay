9401984c02934a42310e5b0a5c0582b89ac215d3
GalijotasE <galijotase@gmail.com>
2022-12-06 14:00:08 +0200

    click here to see recent changes

--- looking_to_stay/looking_to_stay/urls.py
+++ looking_to_stay/looking_to_stay/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('', include('looking_to_stay_app.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('user_profile/', include('user_profile.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
