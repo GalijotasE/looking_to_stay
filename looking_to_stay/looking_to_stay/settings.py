f9f4b1c35ed0bbba1c6bcd77f19bc8b0746bd0ad
GalijotasE <galijotase@gmail.com>
2022-12-06 11:34:05 +0200

    click here to see recent changes

--- looking_to_stay/looking_to_stay/settings.py
+++ looking_to_stay/looking_to_stay/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'looking_to_stay_app',
+    'user_profile',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- looking_to_stay/looking_to_stay/urls.py
+++ looking_to_stay/looking_to_stay/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('looking_to_stay_app.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
